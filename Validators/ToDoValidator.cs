9404146e26750568ace5ebfd9c33e58f96717811
DreKurdeK <radziak.daniel@o2.pl>
2024-11-28 19:48:39 +0100

    Added validation for ToDo model

---
+++ Validators/ToDoValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using TaskManagerAPI.TaskManagerAPI.Models;
+
+namespace TaskManagerAPI.Validators;
+
+public class ToDoValidator : AbstractValidator<ToDo>
+{
+    public ToDoValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Title is required.")
+            .MaximumLength(100).WithMessage("Title cannot be longer than 100 characters.");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.")
+            .MaximumLength(500).WithMessage("Description cannot be longer than 500 characters.");
+
+        RuleFor(x => x.Expiry)
+            .GreaterThan(DateTimeOffset.Now).WithMessage("Expiry Date must be in the future.");
+
+        RuleFor(x => x.PercentComplete)
+            .InclusiveBetween(0, 100).WithMessage("Percent Complete must be between 0 and 100.");
+    }
+}
