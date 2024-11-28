c38be9de99ce83d53238b0c9a1a5cfb0d1583cc1
DreKurdeK <radziak.daniel@o2.pl>
2024-11-28 17:04:02 +0100

    Added DbContext

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerAPI.TaskManagerAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ToDoDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
