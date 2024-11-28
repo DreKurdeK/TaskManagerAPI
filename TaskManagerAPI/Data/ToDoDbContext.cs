bde258a5535deefee428b7c7d8ac0b2153e3b303
DreKurdeK <radziak.daniel@o2.pl>
2024-11-28 17:04:38 +0100

    Added DbContext class

---
+++ TaskManagerAPI/Data/ToDoDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerAPI.TaskManagerAPI.Models;
+
+namespace TaskManagerAPI.TaskManagerAPI.Data;
+
+public class ToDoDbContext(DbContextOptions<ToDoDbContext> options) : DbContext(options)
+{
+    public DbSet<ToDo> ToDos { get; set; }
+}
