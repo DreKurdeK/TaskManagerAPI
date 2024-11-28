7b57d59fb6a00d9954838fe3a67fbfce9b7cf44a
DreKurdeK <radziak.daniel@o2.pl>
2024-11-28 17:10:13 +0100

    Changed file structure

--- TaskManagerAPI/Data/ToDoDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-using TaskManagerAPI.TaskManagerAPI.Models;
-
-namespace TaskManagerAPI.TaskManagerAPI.Data;
-
-public class ToDoDbContext(DbContextOptions<ToDoDbContext> options) : DbContext(options)
-{
-    public DbSet<ToDo> ToDos { get; set; }
-}
