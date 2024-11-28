595110abf0839d7e6bb951ddead6f33e77e20fb0
DreKurdeK <radziak.daniel@o2.pl>
2024-11-28 18:55:52 +0100

    Added Swagger and endpoints

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerAPI.Endpoints;
 using TaskManagerAPI.TaskManagerAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ToDoDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+// Use endpoints for ToDo from different file
+app.MapToDoEndpoints();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapToDoSeederEndpoint();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
