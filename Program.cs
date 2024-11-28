f5ada677994ae5b972d9e948ab0d87b892c55112
DreKurdeK <radziak.daniel@o2.pl>
2024-11-28 19:43:39 +0100

    Added comments and FluentValidation

--- Program.cs
+++ Program.cs
@@ -1,19 +1,29 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
 using TaskManagerAPI.Endpoints;
 using TaskManagerAPI.TaskManagerAPI.Data;
+using TaskManagerAPI.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// DbContext
 builder.Services.AddDbContext<ToDoDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// FluentValidation
+builder.Services.AddValidatorsFromAssemblyContaining<ToDoValidator>();
 
 var app = builder.Build();
 
 // Use endpoints for ToDo from different file
 app.MapToDoEndpoints();
 
+// Swagger and seeder for development only
 if (app.Environment.IsDevelopment())
 {
     app.MapToDoSeederEndpoint();
@@ -21,6 +31,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Default map
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
