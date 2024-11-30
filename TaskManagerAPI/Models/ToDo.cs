85a0c87262712c8115669154759b90f73e70b0e0
DreKurdeK <radziak.daniel@o2.pl>
2024-11-30 12:18:01 +0100

    Added max length attribute to remove warnings

--- TaskManagerAPI/Models/ToDo.cs
+++ TaskManagerAPI/Models/ToDo.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TaskManagerAPI.Models;
 
 public class ToDo
 {
     public Guid Id { get; set; }
+    
+    [MaxLength(100)]
     public required string Title { get; set; }
+    
+    [MaxLength(500)]
     public required string Description { get; set; }
     public DateTimeOffset Expiry { get; set; }
-    public int PercentComplete { get; set; } = 0;
+    public int? PercentComplete { get; set; } = 0;
     public bool? IsDone { get; set; } = false;
 }
