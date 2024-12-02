11b71a3af2ad24abbf2d2384cc9bbb944cc53486
DreKurdeK <radziak.daniel@o2.pl>
2024-12-02 21:48:36 +0100

    Added tests

---
+++ TaskManagerAPI.Tests/AppFactory.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TaskManagerAPI.Data;
+
+namespace TaskManagerAPI.Tests;
+
+public sealed class AppFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            RemoveExistingDbContext(services);
+            services.AddDbContext<ToDoDbContext>(options =>
+                options.UseInMemoryDatabase("todoapp"));
+        });
+    }
+
+    private static void RemoveExistingDbContext(IServiceCollection services)
+    {
+        var descriptor = services.SingleOrDefault(serviceDescriptor =>
+            serviceDescriptor.ServiceType == typeof(DbContextOptions<ToDoDbContext>));
+        
+        if (descriptor != null)
+            services.Remove(descriptor);
+
+        var providerDescriptors = services
+            .Where(serviceDescriptor => serviceDescriptor
+                .ServiceType.FullName?.Contains("EntityFrameworkCore") == true)
+            .ToList();
+        
+        foreach (var providerDescriptor in providerDescriptors)
+            services.Remove(providerDescriptor);
+    }
+}
--- TaskManagerAPI.Tests/ToDoEndpointsTests.cs
+++ TaskManagerAPI.Tests/ToDoEndpointsTests.cs
@@ -1,29 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TaskManagerAPI.Models;
 
 namespace TaskManagerAPI.Tests;
 
-public class ToDoEndpointsTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+public class ToDoEndpointsTests(AppFactory factory) : IClassFixture<AppFactory>
 {
-    private readonly HttpClient _client = factory.CreateClient();
-
-    [Fact]
-    public async Task GetAllToDos_ShouldReturnOk_WhenThereAreTodos()
-    {   
-        var response = await _client.GetAsync("/todos");
-        
-        response.EnsureSuccessStatusCode();
-        if (response.Content.Headers.ContentType != null)
-            Assert.Equal("application/json", response.Content.Headers.ContentType.MediaType);
-    }
-
     [Fact]
     public async Task GetAllToDos_ShouldReturnEmptyList_WhenNoTodosExist()
     {
-        var response = await _client.GetAsync("/todos");
+        // arrange
+        var client = factory.CreateClient(); 
+            
+        // act
+        var response = await client.GetAsync("/todos");
         
+        // assert
         response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
-        
-        Assert.Equal("[]", content);
+        var todos = await response.Content.ReadFromJsonAsync<IEnumerable<ToDo>>();
+        Assert.NotNull(todos);
+        Assert.Empty(todos);
     }
+    
+    [Fact]
+    public async Task GetAllToDos_ShouldReturnOk_WhenThereAreTodos()
+    {   
+        // arrange
+        var client = factory.CreateClient();
+        await CreateTodo(client, "Test 1", "Test 1 desc");
+        await CreateTodo(client, "Test 2", "Test 2 desc");
+        
+        // act
+        var response = await client.GetAsync("/todos");
+        
+        // assert
+        response.EnsureSuccessStatusCode();
+        var todos = (await response.Content.ReadFromJsonAsync<IEnumerable<ToDo>>())?.ToArray();
+        Assert.NotNull(todos);
+        Assert.Equal(2, todos.Length);
+        Assert.Contains(todos, t => t.Title == "Test 1");
+        Assert.Contains(todos, t => t.Title == "Test 2");
+    }
+
+    private static async Task CreateTodo(HttpClient client, string title, string description)
+    {
+        var toDo = new ToDo
+        {
+            Title = title,
+            Description = description,
+            Expiry = DateTimeOffset.Now.AddDays(7)
+        };
+        var creatingResponse = await client.PostAsJsonAsync("/todos", toDo);
+        creatingResponse.EnsureSuccessStatusCode();
+    }
+
+
 }
