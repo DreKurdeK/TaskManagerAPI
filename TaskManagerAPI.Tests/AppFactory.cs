ead13ce9e0be5055bffd34b4227aa903145c8add
DreKurdeK <radziak.daniel@o2.pl>
2024-12-04 11:21:54 +0100

    Made tests work in Test Container

--- TaskManagerAPI.Tests/AppFactory.cs
+++ TaskManagerAPI.Tests/AppFactory.cs
@@ -1,66 +1,43 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using TaskManagerAPI.Data;
 using Testcontainers.PostgreSql;
-using Npgsql;
+using TaskManagerAPI.DAL;
 
-namespace TaskManagerAPI.Tests
+namespace TaskManagerAPI.Tests;
+
+public sealed class AppFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
-    public sealed class AppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    private readonly PostgreSqlContainer _databaseContainer = new PostgreSqlBuilder()
+        .WithUsername("postgres")
+        .WithPassword("postgres")
+        .WithDatabase("todoapp")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder) => builder
+        .ConfigureAppConfiguration(configurationBuilder => configurationBuilder
+            .AddInMemoryCollection([
+                new(
+                    key: "ConnectionStrings:DefaultConnection",
+                    _databaseContainer.GetConnectionString())
+            ]));
+
+    public async Task InitializeAsync()
     {
-        private readonly PostgreSqlContainer _databaseContainer = new PostgreSqlBuilder()
-            .WithUsername("postgres")
-            .WithPassword("postgres")
-            .WithDatabase("todoapp")
-            .Build();
+        await _databaseContainer.StartAsync();
+        await MigrateDb();
+    }
 
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
-        {
-            builder.ConfigureServices(services =>
-            {
-                RemoveDbContextConfiguration(services);
+    public new async Task DisposeAsync() => 
+        await _databaseContainer.StopAsync();
 
-                services.AddDbContext<ToDoDbContext>(options =>
-                {
-                    options.UseNpgsql(_databaseContainer.GetConnectionString());
-                });
-            });
-        }
-
-        private static void RemoveDbContextConfiguration(IServiceCollection services)
-        {
-            var dbContextDescriptor = services
-                .SingleOrDefault(descriptor => descriptor.ServiceType == typeof(DbContextOptions<ToDoDbContext>));
-            
-            if (dbContextDescriptor != null)
-                services.Remove(dbContextDescriptor);
-
-            var efCoreDescriptors = services
-                .Where(descriptor => descriptor.ServiceType.FullName?.Contains("EntityFrameworkCore") == true)
-                .ToList();
-            
-            foreach (var descriptor in efCoreDescriptors)
-                services.Remove(descriptor);
-        }
-
-        public async Task InitializeAsync()
-        {
-            await _databaseContainer.StartAsync();
-
-            var connectionString = _databaseContainer.GetConnectionString();
-            await using var connection = new NpgsqlConnection(connectionString);
-            await connection.OpenAsync();
-
-            using var scope = Services.CreateScope();
-            var dbContext = scope.ServiceProvider.GetRequiredService<ToDoDbContext>();
-            await dbContext.Database.MigrateAsync();
-        }
-
-        public new async Task DisposeAsync()
-        {
-            await _databaseContainer.StopAsync();
-        }
+    private async Task MigrateDb()
+    {
+        using var scope = Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<ToDoDbContext>();
+        if ((await db.Database.GetPendingMigrationsAsync()).Any())
+            await db.Database.MigrateAsync();
     }
 }
